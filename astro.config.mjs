ca8acb537ded00751645910ac054f06c2d3a0410
mendoza000 <omarmendoza.alv25@gmail.com>
2023-12-15 11:54:27 -0400

    Add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@vercel/analytics";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+	output: "server",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+	integrations: [tailwind(), react()],
 });
