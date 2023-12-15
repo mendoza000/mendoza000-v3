f2fe60caa87e60a940deca1eab09ec9563d249ca
mendoza000 <omarmendoza.alv25@gmail.com>
2023-12-15 12:17:09 -0400

    fix??

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react()],
-	output: "server",
+	output: "static",
 	adapter: vercel({
 		webAnalytics: {
 			enabled: true,
