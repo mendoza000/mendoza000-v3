7e0f9611e04663a2691bb634b5161d50d816684f
mendoza000 <omarmendoza.alv25@gmail.com>
2023-12-15 12:22:42 -0400

    fix :c?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/static";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react()],
-	output: "static",
+	output: "server",
 	adapter: vercel({
 		webAnalytics: {
 			enabled: true,
