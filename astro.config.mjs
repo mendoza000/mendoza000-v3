09fda7f9cd5791abccb0552a5e316b55f13ad4c0
mendoza000 <omarmendoza.alv25@gmail.com>
2023-12-28 13:30:23 -0400

    add hybrid output and vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-// import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react()],
-	// output: "server",
-	// adapter: vercel({
-	// 	webAnalytics: {
-	// 		enabled: true,
-	// 	},
-	// }),
+	output: "hybrid",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 });
