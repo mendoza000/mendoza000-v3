e4b5d7c84ebc8465ae4d7459f07ea09063a7d634
mendoza000 <omarmendoza.alv25@gmail.com>
2024-02-17 14:42:55 -0400

    Actualizar componente Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,12 +4,15 @@ interface Props {
 	variant?: "secondary" | "primary" | "destroy" | "outline";
 	action?: () => void;
 	children: string | JSX.Element | JSX.Element[];
+	class?: string | JSX.ElementClass;
 }
 
 const Button = (props: Props) => {
 	return (
 		<button
 			className={`px-4 py-2 bg-green-800 rounded-2xl gap-2 hover:gap-3 text-center hover:bg-green-700 duration-200 font-semibold flex ${
+				props.class
+			} ${
 				props.variant === "outline"
 					? "bg-transparent border-2 border-green-800 dark:text-green-500 text-green-800 hover:bg-transparent hover:border-green-700"
 					: props.variant === "destroy"
