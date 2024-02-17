fa7e7f41809eee47a88fad5de0ce69a5e9a081b1
mendoza000 <omarmendoza.alv25@gmail.com>
2024-02-17 15:21:50 -0400

    enhanced navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,6 +3,12 @@ import { Moon, MenuIcon } from "lucide-react";
 import NavbarItem from "./NavbarItem.tsx";
 import ToggleTheme from "./ToggleTheme.tsx";
 
+// className={`lg:px-10 px-5 py-3 lg:py-4 fixed top-0 left-0 min-w-full flex flex-col lg:flex-row lg:justify-between z-30 duration-200 bg-zinc-800 dark:backdrop-blur-xl lg:dark:bg-opacity-100 ${
+// 	openMenu
+// 		? "min-h-screen bg-opacity-90 dark:bg-opacity-90 text-white"
+// 		: " min-h-0 bg-opacity-20 dark:bg-opacity-50"
+// }`}
+
 const Navbar = () => {
 	const [openMenu, setOpenMenu] = useState(false);
 
@@ -12,51 +18,53 @@ const Navbar = () => {
 
 	return (
 		<nav
-			className={`lg:px-10 px-5 py-3 lg:py-4 fixed top-0 left-0 min-w-full flex flex-col lg:flex-row lg:justify-between z-30 bg-zinc-900 duration-200 lg:bg-opacity-0 lg:dark:bg-opacity-0 ${
+			className={`fixed top-0 min-w-full bg-zinc-800/50 py-2 backdrop-blur-xl z-10 ${
 				openMenu
 					? "min-h-screen bg-opacity-90 dark:bg-opacity-90 text-white"
 					: " min-h-0 bg-opacity-20 dark:bg-opacity-50"
 			}`}
 		>
-			<button disabled className="opacity-0 px-3 border-2 hidden lg:flex">
-				<Moon />
-			</button>
-
-			<div
-				className={`flex justify-between min-w-full lg:hidden duration-300 ${
-					openMenu
-						? "bg-zinc-900 bg-opacity-75 py-2 px-2 rounded-2xl shadow-xl"
-						: "bg-transparent p-0 rounded-none shadow-none"
-				}`}
-			>
-				<ToggleTheme />
-
-				<button
-					className="px-2 lg:hidden items-center justify-center inline-flex max-h-[2.5rem] duration-200"
-					onClick={handleOpenMenu}
-				>
-					<MenuIcon />
+			<div className="flex justify-between px-5 max-w-7xl mx-auto flex-col lg:flex-row">
+				<button disabled className="opacity-0 px-3 border-2 hidden lg:flex">
+					<Moon />
 				</button>
-			</div>
 
-			<ul
-				className={` lg:rounded-xl lg:px-2 py-4 lg:py-2 lg:shadow-xl dark:shadow-lg lg:bg-green-800 lg:bg-opacity-10 gap-5 lg:backdrop-blur-md lg:flex ${
-					!openMenu ? "hidden" : "flex flex-col"
-				}`}
-			>
-				<NavbarItem href="#" text="Inicio" />
-				<NavbarItem href="#experiences" text="Experiencia" />
-				<NavbarItem href="#projects" text="Proyectos" />
-				<NavbarItem href="#contact" text="Contacto" />
-				<NavbarItem
-					href="https://devstopia.vercel.app"
-					target="_blank"
-					text="Blog"
-				/>
-			</ul>
+				<div
+					className={`flex justify-between min-w-full lg:hidden duration-300 text-white ${
+						openMenu
+							? "bg-zinc-900 bg-opacity-75 py-2 px-2 rounded-2xl shadow-xl"
+							: "bg-transparent p-0 rounded-none shadow-none"
+					}`}
+				>
+					<ToggleTheme />
 
-			<div className="hidden lg:flex">
-				<ToggleTheme />
+					<button
+						className="px-2 lg:hidden items-center justify-center inline-flex max-h-[2.5rem] duration-200"
+						onClick={handleOpenMenu}
+					>
+						<MenuIcon />
+					</button>
+				</div>
+
+				<ul
+					className={` lg:rounded-xl lg:px-2 py-4 lg:py-2  gap-5  lg:flex text-white ${
+						!openMenu ? "hidden" : "flex flex-col"
+					}`}
+				>
+					<NavbarItem href="#" text="Inicio" />
+					<NavbarItem href="#experiences" text="Experiencia" />
+					<NavbarItem href="#projects" text="Proyectos" />
+					<NavbarItem href="#contact" text="Contacto" />
+					<NavbarItem
+						href="https://devstopia.vercel.app"
+						target="_blank"
+						text="Blog"
+					/>
+				</ul>
+
+				<div className="hidden lg:flex text-white">
+					<ToggleTheme />
+				</div>
 			</div>
 		</nav>
 	);
