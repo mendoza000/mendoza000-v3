8430c98d7a9fc6508e138a8388294a43be99cb32
mendoza000 <omarmendoza.alv25@gmail.com>
2024-02-18 12:43:19 -0400

    change border radius

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -10,7 +10,7 @@ interface Props {
 const Button = (props: Props) => {
 	return (
 		<button
-			className={`px-4 py-2 bg-green-800 rounded-2xl gap-2 hover:gap-3 text-center hover:bg-green-700 duration-200 font-semibold flex ${
+			className={`px-4 py-2 bg-green-800 rounded-lg gap-2 hover:gap-3 text-center hover:bg-green-700 duration-200 font-semibold flex ${
 				props.class
 			} ${
 				props.variant === "outline"
